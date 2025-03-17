b2f6341fa9a1e106e000f789493ea12c7977b035
Peatro <PeatroXD@yandex.ru>
2025-03-17 21:49:17 +0300

    Добавлена реализация сортировки слиянием в класс CustomArrayList

--- Homework02/src/main/java/org/example/CustomArrayList.java
+++ Homework02/src/main/java/org/example/CustomArrayList.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class CustomArrayList<E> {
+public class CustomArrayList<E extends Comparable<E>> {
 
     private final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
@@ -91,6 +91,51 @@ public class CustomArrayList<E> {
     private void checkIndex(int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index + " Size: " + size);
+        }
+    }
+
+    public void mergeSort() {
+        if (size <= 1) {
+            return;
+        }
+        mergeSort(0, size - 1);
+    }
+
+    private void mergeSort(int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+            merge(left, mid, right);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void merge(int left, int mid, int right) {
+        Object[] leftArray = Arrays.copyOfRange(elements, left, mid + 1);
+        Object[] rightArray = Arrays.copyOfRange(elements, mid + 1, right + 1);
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < leftArray.length && j < rightArray.length) {
+            E leftElement = (E) leftArray[i];
+            E rightElement = (E) rightArray[j];
+
+            if (leftElement.compareTo(rightElement) <= 0) {
+                elements[k++] = leftArray[i++];
+            } else {
+                elements[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < leftArray.length) {
+            elements[k++] = leftArray[i++];
+        }
+
+        while (j < rightArray.length) {
+            elements[k++] = rightArray[j++];
         }
     }
 }
